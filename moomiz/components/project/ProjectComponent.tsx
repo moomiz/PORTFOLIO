a9f28e1df0ea77acce1f6f4dd40a973b960b3e9d
Minji Kim <moo_miz@naver.com>
2023-06-30 01:22:16 +0900

    Feat(fe): project img setting

--- moomiz/components/project/ProjectComponent.tsx
+++ moomiz/components/project/ProjectComponent.tsx
@@ -3,49 +3,74 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import GHEM from '@/public/GHEM.gif'
 import PITCHIT from '@/public/pitchit.png'
+import Nevvel from '@/public/Nevvel.png'
 
 function ProjectComponent() {
   return (
     <Wrapper>
       <Title>Project</Title>
       <ProjectList>
+        <ProjectItem>
         <Image 
         src={GHEM}
         alt='GHEM'
-        height={240}
+        height={280}
         width={480}
         />
+        <ProjectTitle>GHEM</ProjectTitle>
+        </ProjectItem>
+        <ProjectItem>
         <Image 
         src={PITCHIT}
         alt='PITCHIT'
-        height={240}
+        height={280}
         width={480}
         />
-        <div>
-          PITCHIT</div>
-        <div>GHEM</div>
-        <div>Nevvel</div>
+        <ProjectTitle>PITCHIT</ProjectTitle>
+        </ProjectItem>
+        <ProjectItem>
+        <Image 
+        src={Nevvel}
+        alt='Nevvel'
+        height={280}
+        width={480}
+        />
+        <ProjectTitle>Nevvel</ProjectTitle>
+        </ProjectItem>
       </ProjectList>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-height: 100%;
+  height: 100%;
+  width: 100%;
   color:white;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-size: 50px;
 `
 const Title = styled.div`
   padding-bottom: 5%;
 `
 const ProjectList = styled.div`
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+`
+const ProjectItem = styled.div`
+    width: 100%;
+    display: flex;
+  padding: 5%;
+  justify-content: space-between;
+  align-items: center;
+`
+const ProjectTitle = styled.div`
+  
 `
 
 export default ProjectComponent
